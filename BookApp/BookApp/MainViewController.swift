fa680fbf405c20c4b111a14cd7a54078b6a8b6bb
UkhyunGo <dnrwk99@gmail.com>
2025-05-09 20:38:21 +0900

    feat: Main.storyboard 제거

---
+++ BookApp/BookApp/MainViewController.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+
+
+}
+
