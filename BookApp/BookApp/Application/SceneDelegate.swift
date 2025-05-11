2383c874b3df5cef58638b08fbc09979ee381e36
UkhyunGo <dnrwk99@gmail.com>
2025-05-11 21:14:12 +0900

    feat: UITabBarController 구현

--- BookApp/BookApp/Application/SceneDelegate.swift
+++ BookApp/BookApp/Application/SceneDelegate.swift
@@ -7,7 +7,27 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = MainViewController()
+        
+        let mainVC = UINavigationController(rootViewController: MainViewController())
+        let listVC = UINavigationController(rootViewController: BookListViewController())
+        
+        let tabBarController = UITabBarController()
+        
+        tabBarController.setViewControllers([mainVC, listVC], animated: true)
+        
+        tabBarController.tabBar.tintColor = .black
+        tabBarController.tabBar.unselectedItemTintColor = .darkGray
+        
+        if let items = tabBarController.tabBar.items {
+            items[0].selectedImage = UIImage(systemName: "magnifyingglass.circle.fill")
+            items[0].image = UIImage(systemName: "magnifyingglass.circle")
+            items[0].title = "책 검색"
+            
+            items[1].selectedImage = UIImage(systemName: "folder.fill")
+            items[1].image = UIImage(systemName: "folder")
+            items[1].title = "책 리스트"
+        }
+        window?.rootViewController = tabBarController
         window?.makeKeyAndVisible()
     }
 
--- BookApp/BookApp/View/BookListViewController.swift
+++ BookApp/BookApp/View/BookListViewController.swift
@@ -1,15 +1,12 @@
 import UIKit
 import SnapKit
 
-class MainViewController: UIViewController {
+class BookListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
     }
-    
-    
 
 }
-
