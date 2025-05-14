07f3527cdca39e55006de4fc2132092369723dd2
UkhyunGo <dnrwk99@gmail.com>
2025-05-15 00:52:49 +0900

    feat: ViewModel 기본 골자 구현(Input - Output 패턴)

--- BookApp/BookApp/ViewModel/MainViewModel.swift
+++ BookApp/BookApp/ViewModel/MainViewModel.swift
@@ -1,1 +1,25 @@
 import Foundation
+import RxSwift
+
+protocol MainViewModelProtocol: AnyObject {
+    associatedtype Input
+    associatedtype Output
+    
+    func transform(with: Input) -> Output
+}
+
+class MainViewModel: MainViewModelProtocol {
+    
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+    
+    func transform(with: Input) -> Output {
+        <#code#>
+    }
+
+}
